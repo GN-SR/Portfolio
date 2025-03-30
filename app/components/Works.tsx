38a6eb9f0354dbe962cf86fffdac1c4b86c68b58
g-n-s-r <147964739+g-n-s-r@users.noreply.github.com>
2025-03-30 17:01:05 +0530

    More Changes

--- app/components/Works.tsx
+++ app/components/Works.tsx
@@ -114,11 +114,9 @@ const Works = () => {
 					variants={fadeIn("", "", 0.1, 1)}
 					className="mt-3 text-secondary text-[17px] max-w-3xl leading-[30px]"
 				>
-					Following projects showcases my skills and experience through
-					real-world examples of my work. Each project is briefly described with
-					links to code repositories and live demos in it. It reflects my
-					ability to solve complex problems, work with different technologies,
-					and manage projects effectively.
+					The following projects highlight my skills and experience through real-world applications. 
+					Each project includes a brief description along with links to the code repositories. 
+					These projects demonstrate my ability to solve complex problems, work with various technologies, and build efficient software solutions.
 				</motion.p>
 			</div>
 
--- app/constants/index.ts
+++ app/constants/index.ts
@@ -89,41 +89,16 @@ const technologies = [
 
 const experiences = [
 	{
-		title: "Wordpress Developer & UI/UX Designer",
-		company_name: "A.V.M Auto",
-		icon: "/company/avm.webp",
-		iconBg: "#383E56",
-		date: "2020 -  2023",
+		title: "Full Stack Developer Intern",
+		company_name: "Basal Analytics",
+		icon: "/company/Basal.webp",
+		iconBg: "#FFFFFF",
+		date: "Sep 2024 -  Dec 2024",
 		points: [
-			"Self-Taught Expertise: Gained proficiency in WordPress and UI/UX design through independent learning.",
-			"Collaborative Design: Worked closely with stakeholders to gather feedback and iterate on the portfolio website's interactivity.",
-			"Successful Website Development: Designed and developed a visually appealing and user-friendly portfolio website using WordPress.",
-			"Positive Business Impact: Contributed to +54.25% sales and overall business growth through the improved website."
-		],
-	},
-	{
-		title: "Next.JS Developer",
-		company_name: "Sparkbright Engineering",
-		icon: "/company/sparkbright.webp",
-		iconBg: "#E6DEDD",
-		date: "2023 - 2024",
-		points: [
-			"Self-Taught Expertise: Gained proficiency in React.JS and Next.JS through independent learning.",
-			"Successful Website Development: Designed and developed a visually appealing and user-friendly portfolio website using Next.JS.",
-			"Collaborative Design: Worked closely with stakeholders to gather feedback and iterate on the portfolio website's interactivity."
-		],
-	},
-	{
-		title: "Software Developer",
-		company_name: "Way to Web Pvt. Ltd.",
-		icon: "/company/wtw.jpg",
-		iconBg: "#E6DEDD",
-		date: "2024 - Present",
-		points: [
-			"Developing and maintaining Wordpress Site for A.V.M Auto The Car Body Shop based in Ahmedabad, Gujarat India.",
-			"Analyzing feedbacks and fixing bugs.",
-			"Search Engine Optimization.",
-			"Design Maintainence and hosting maintenance.",
+			"Real-Time Development: Built a product classification and inventory management system using Flask and SQLite3.",
+			"Seamless Integration: Ensured smooth frontend-backend interaction for an intuitive user experience.",
+			"Efficient Database Management: Implemented real-time inventory tracking and updates to enhance operational efficiency.",
+			"Hands-on Experience: Strengthened full-stack development skills by working on a practical, real-world project."
 		],
 	},
 ];
@@ -132,42 +107,18 @@ const testimonials = [
 	{
 		id: 1,
 		testimonial:
-			"Behance is a social media platform owned by Adobe whose main focus is to showcase and discover creative work.",
-		name: "Om Patel",
-		image: "/socialmedia/behance.svg",
-		link: "https://www.behance.net/omthecreator",
+			"Check out my LinkedIn profile for more information about my experience and skills.",
+		name: "Guna Swaroop",
+		image: "/socialmedia/linkedin.svg",
+		link: "https://www.linkedin.com/in/guna-swaroop/",
 	},
 	{
 		id: 2,
 		testimonial:
-			"LinkedIn is a business and employment-focused social media platform that works through websites and mobile apps.",
-		name: "Om Patel",
-		image: "/socialmedia/linkedin.svg",
-		link: "https://www.linkedin.com/in/om-patel-401068143/",
-	},
-	{
-		id: 3,
-		testimonial:
-			"Dribbble is a self-promotion and social networking platform for digital designers and creatives. It serves as a design portfolio.",
-		name: "Om Patel",
-		image: "/socialmedia/dribble.svg",
-		link: "https://dribbble.com/om2121",
-	},
-	{
-		id: 4,
-		testimonial:
-			"Also do check out my UI/UX Portfolio where I have shared by design studies.",
-		name: "Om Patel",
-		image: "/socialmedia/portfolio.svg",
-		link: "https://omthecreator.netlify.app/",
-	},
-	{
-		id: 5,
-		testimonial:
-			"Also do check out my Github Profile where I have shared all my codes from basic to advanced.",
-		name: "Om Patel",
+			"Also do check out my Github Profile where I have shared all my Projects.",
+		name: "Guna Swaroop ",
 		image: "/tech/github.webp",
-		link: "https://github.com/omunite215",
+		link: "https://github.com/GN-SR",
 	},
 ];
 
